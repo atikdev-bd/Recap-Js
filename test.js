d83c8886f1eb57d95769bf30d7677e73fe37f980
“atikdev-bd” <atikdev.bd@gmail.com>
2023-09-12 15:54:43 +0600

    recap javaScript

---
+++ test.js
@@ -0,0 +1,20 @@
+
+// javascript Assengment
+
+let x = 5;
+x += 10;
+console.log(x);
+
+let y = 20;
+y -= 4;
+console.log(y);
+
+let z = 20;
+
+z *= 2;
+console.log(z)
+let a = 40;
+
+a /= 5;
+
+console.log(a)
