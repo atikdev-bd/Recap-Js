10737d140cb2b6e0ae04de8ba45741ce2c374d26
“atikdev-bd” <atikdev.bd@gmail.com>
2023-09-24 21:11:21 +0600

    Es6 const emplement

---
+++ arrayConstant.js
@@ -0,0 +1,11 @@
+// you can't reasince const elemet but you can change array element //
+const car = ["Toyota", "Volvo", "BMW"];
+
+car[0] = "Tesla";
+
+
+/// you can push extra element in const array or objects//
+
+car.push("Safari");
+
+console.log(car);
