5f776df5c244330de4a5dfe19704db759915f688
“atikdev-bd” <atikdev.bd@gmail.com>
2023-09-25 21:13:09 +0600

    whileLoop practice

---
+++ forLoop.js
@@ -0,0 +1,5 @@
+let car = ["A", "B", "C", "D", "E"];
+
+for (let i = 0; i < car.length; i++) {
+  console.log(i);
+}
---
+++ whileLoop.js
@@ -0,0 +1,11 @@
+let i = 0;
+let text = "";
+
+while (i > 10) {
+  text += "The number is " + i;
+    i++;
+    console.log(text);
+}
+
+
+
