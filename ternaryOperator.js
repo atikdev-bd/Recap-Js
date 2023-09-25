56b511831049bbf7c0e5586736edf6ec6f0ce306
“atikdev-bd” <atikdev.bd@gmail.com>
2023-09-25 11:04:54 +0600

    ternary operator

---
+++ ternaryOperator.js
@@ -0,0 +1,16 @@
+let age = 18;
+
+let age2 = 14;
+
+if (age2 >= 18) {
+  console.log("You are Adult");
+} else {
+  console.log("You are not Adult");
+}
+
+
+/// ternary operator is use to shortcat in if / else // 
+
+let result = age >= 18 ? "you are adult" : "you are not adult";
+
+console.log(result);
